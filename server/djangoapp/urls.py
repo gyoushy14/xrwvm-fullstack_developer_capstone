d19c0aab42cdf7f1be0d1a68b911946f38d03a62
gyuoshy14 <gyuoshy1417@gmail.com>
2024-08-15 17:12:14 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # # path for registration
 
     # path for login
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_view, name='logout'),
     path(route='registration', view=views.registration, name='registration'),
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://gyuoshy1417-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://gyuoshy1417-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['gyuoshy1417-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://gyuoshy1417-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
